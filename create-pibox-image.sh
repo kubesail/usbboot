8654ad36dacb81f80977bb71b9646e7f915ef362
Dan Pastusek <dan@hexial.com>
2022-03-16 19:20:04 -0600

    add pibox image scripts

---
+++ create-pibox-image.sh
@@ -0,0 +1,1 @@
+sudo dd if=/dev/sdb of=pibox-os.iso bs=4M conv=sparse status=progress oflag=direct
---
+++ pibox-bootloader-loop.sh
@@ -0,0 +1,6 @@
+while true; do
+	sudo ./rpiboot -d pibox
+	clear
+	echo "Flashing complete!\n\n\n"
+done
+
